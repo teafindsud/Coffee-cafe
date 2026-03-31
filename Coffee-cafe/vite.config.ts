0dded9653950d4cefca18695ed2a4a1d6e3e7aa2
Utsav Gupta <utg18688@gmail.com>
2026-04-01 04:28:18 +0530

    final

--- Coffee-cafe/vite.config.ts
+++ Coffee-cafe/vite.config.ts
@@ -4,15 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    drop: ['console', 'debugger'],
+  },
   build: {
     outDir: 'dist',
     sourcemap: false,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-      },
-    },
     reportCompressedSize: true,
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
